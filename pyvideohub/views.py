3fd14d1021269d1dc82367d3f79cc24c1f4e71c8
William Wu <william@pylabs.net>
2014-02-26 18:38:38 +0800

    建立各個對應的 view

--- pyvideohub/views.py
+++ pyvideohub/views.py
@@ -2,5 +2,17 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='pyvideohub:templates/home.jinja2')
-def my_view(request):
+def home_view(request):
+    return {}
+
+@view_config(route_name='upload', renderer='pyvideohub:templates/upload.jinja2')
+def upload_view(request):
+    return {}
+
+@view_config(route_name='about', renderer='pyvideohub:templates/about.jinja2')
+def about_view(request):
+    return {}
+
+@view_config(route_name='contact_us', renderer='pyvideohub:templates/contact_us.jinja2')
+def contact_us_view(request):
     return {}
