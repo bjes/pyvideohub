a146873b478212b9322579ae445516a419bb4f97
William Wu <william@pylabs.net>
2014-03-04 15:52:05 +0800

    啟用 pyramid 內建的 session 機制

--- pyvideohub/__init__.py
+++ pyvideohub/__init__.py
@@ -1,10 +1,12 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    session_factory = SignedCookieSessionFactory(settings['session.secret'])
+    config = Configurator(settings=settings, session_factory=session_factory)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
