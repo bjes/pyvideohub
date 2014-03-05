ae670b913bd6359f1ebc6bec5dfcb7e40469133d
William Wu <william@pylabs.net>
2014-03-05 14:33:12 +0800

    完成影片上傳功能，並整合了 WTForms 與 SQLAlchemy

--- pyvideohub/forms.py
+++ pyvideohub/forms.py
@@ -1,17 +1,14 @@
-import colander
-import deform
-from deform.schema import FileData
-from deform.widget import FileUploadWidget
+from wtforms import Form, StringField, TextAreaField, FileField, validators
 
-class _TmpStore(dict):
-    def preview_url(self, name):
-        return None
+class UploadForm(Form):
+    title = StringField('影片標題',
+        [validators.InputRequired(message='影片標題為必要欄位'),
+         validators.Length(max=100, message='文字長度限制在 100 字以下')])
+    description = TextAreaField('影片說明',
+        [validators.Length(max=65535, message='文字長度限制在 65535 字以下')])
+    file = FileField('影片檔案')
 
-class UploadSchema(colander.MappingSchema):
-    title = colander.SchemaNode(colander.String(),
-        validator=colander.Length(max=100),
-        widget=deform.widget.TextInputWidget())
-    description = colander.SchemaNode(colander.String(),
-        validator=colander.Length(max=250),
-        widget=deform.widget.TextAreaWidget(rows=10))
-    file = colander.SchemaNode(FileData(), widget=FileUploadWidget(_TmpStore()))
+    def validate_file(form, field):
+        from cgi import FieldStorage
+        if not isinstance(field.data, FieldStorage):
+            raise validators.ValidationError('影片檔案為必要欄位')
--- pyvideohub/views.py
+++ pyvideohub/views.py
@@ -1,16 +1,61 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 from . import forms
-from deform import Form
-
+from .models import DBSession
 
 @view_config(route_name='home', renderer='pyvideohub:templates/home.jinja2')
 def home_view(request):
     return {}
 
-@view_config(route_name='upload', renderer='pyvideohub:templates/upload.jinja2')
-def upload_view(request):
-    form = Form(forms.UploadSchema())
+@view_config(route_name='upload', renderer='pyvideohub:templates/upload.jinja2',
+             request_method='GET')
+def upload_view_via_get(request):
+    form = forms.UploadForm()
     return {'form': form}
+
+@view_config(route_name='upload', renderer='pyvideohub:templates/upload.jinja2',
+             request_method='POST')
+def upload_view_via_post(request):
+    import os, datetime
+    from .models import Video
+    form = forms.UploadForm(request.POST)
+    if form.validate():
+        with DBSession() as db:
+            new_video = Video()
+            form.populate_obj(new_video)
+            db.add(new_video)
+            db.commit()
+
+            # 將檔案寫入 video.store_path 路徑
+            today = datetime.datetime.today()
+            store_path = request.registry.settings['video.store_path']
+            # tmp- 開頭的檔案接是使用者傳上來的原始檔案，之後轉檔後的檔名會去掉 tmp- 開頭
+            # 原始檔案也會刪掉
+            new_file_name = ''.join(['tmp-', str(new_video.id),
+                                    os.path.splitext(request.POST[form.file.name].filename)[1]])
+            dst_path_basedir = os.path.join(store_path, str(today.year), str(today.month))
+            dst_path = os.path.join(dst_path_basedir, new_file_name)
+            if not os.path.exists(dst_path_basedir):
+                try:
+                    os.makedirs(dst_path_basedir)
+                except OSError:
+                    # Race condition handling. os.mkdirs will raise OSError while the dir has already
+                    # existed.
+                    pass
+            output_file = open(dst_path, 'wb')
+            input_file = request.POST[form.file.name].file
+            input_file.seek(0)
+            while True:
+                content = input_file.read(5120000)
+                if not content:
+                    break
+                output_file.write(content)
+            output_file.close()
+
+            request.session.flash('新增影片成功')
+            return HTTPFound('/')
+    else:
+        return {'form': form}
 
 @view_config(route_name='about', renderer='pyvideohub:templates/about.jinja2')
 def about_view(request):
